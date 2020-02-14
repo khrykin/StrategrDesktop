3df5df545122a0429e8142c9c3d8f895185ba77d
Dmitry Khrykin <khrykin@me.com>
2020-02-14 19:37:00 +0300

    fixed update appcast script

--- scripts/update_remote_appcast.sh
+++ scripts/update_remote_appcast.sh
@@ -1,6 +1,17 @@
+exit_on_error() {
+    exit_code=$1
+    # shellcheck disable=SC2124
+    last_command=${@:2}
+    if [ "$exit_code" -ne 0 ]; then
+        >&2 echo "\"${last_command}\" command failed with exit code ${exit_code}."
+        exit "$exit_code"
+    fi
+}
+
 echo  "Updating remote appcast..."
 
 git checkout gh-pages
+exit_on_error $? !!
 git add _include/ appcast.xml
 git commit  --amend --no-edit
 git push -f origin gh-pages
